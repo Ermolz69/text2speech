d3834ab1d78387072de8198118d5d1cd14aaf261
Ermolz <00ermzahar@gmail.com>
2026-03-28 19:53:28 +0300

    Fix ESLint 9 config and TypeScript lint dependencies

---
+++ eslint.config.cjs
@@ -0,0 +1,36 @@
+const js = require("@eslint/js");
+const globals = require("globals");
+const tseslint = require("typescript-eslint");
+const prettier = require("eslint-config-prettier");
+
+module.exports = tseslint.config(
+  {
+    ignores: ["**/dist/**", "**/node_modules/**"]
+  },
+  {
+    files: ["src/apps/gateway/src/**/*.ts", "src/shared/src/**/*.ts"],
+    extends: [js.configs.recommended, ...tseslint.configs.recommended, prettier],
+    languageOptions: {
+      ecmaVersion: "latest",
+      sourceType: "module",
+      globals: globals.node
+    }
+  },
+  {
+    files: ["src/apps/web/src/**/*.{ts,tsx}"],
+    extends: [js.configs.recommended, ...tseslint.configs.recommended, prettier],
+    languageOptions: {
+      ecmaVersion: "latest",
+      sourceType: "module",
+      parserOptions: {
+        ecmaFeatures: {
+          jsx: true
+        }
+      },
+      globals: {
+        ...globals.browser,
+        ...globals.node
+      }
+    }
+  }
+);
--- src/apps/gateway/src/app.ts
+++ src/apps/gateway/src/app.ts
@@ -16,8 +16,6 @@ async function start() {
 }
 
 start().catch((err) => {
-  // eslint-disable-next-line no-console
   console.error(err);
   process.exit(1);
 });
-
